5118f273441ad72f2b01da316966d3b0fcf8ac54
Asrith-Ladi <asrith.ladi@gmail.com>
2025-03-30 17:24:59 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     # exc_tb contains all the information like on which exception occured
